802a7001fde5ef5dba2b745056047621dd8844c4
Pritesh Suvagiya <pritesh@taglineinfotech.com>
2022-02-24 17:12:58 +0530

    Create view for conversion and add request rate limit

--- converter/views.py
+++ converter/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+import requests
+from django.http import JsonResponse
+from rest_framework.decorators import (api_view, permission_classes,
+                                       throttle_classes)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.throttling import UserRateThrottle
+
+
+@api_view(['GET', 'POST'])
+@throttle_classes([UserRateThrottle])
+@permission_classes((IsAuthenticated, ))
+def converter(request):
+    converted_amount = None
+    try:
+        data = {
+            "from": request.GET.get('from'),
+            "to": request.GET.get('to'),
+            "amount": request.GET.get('amount')
+        }
+        params = {
+            'currency': data['from'],
+        }
+        res = requests.get(
+            'https://api.coinbase.com/v2/exchange-rates', params=params, stream=True)
+        to = data['to']
+        rate = (res.json()['data']['rates'][to])
+        converted_amount = (float(data['amount'])*float(rate))
+        return JsonResponse({"converted_amount:": converted_amount})
+    except Exception as e:
+        logging.info(e)
+    return JsonResponse({"converted_amount:": converted_amount})
--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'converter',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +102,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+        
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '100/day'
+    }
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
@@ -122,3 +139,36 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=20),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
