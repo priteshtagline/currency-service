2aafa345655bfe6a5db50222c1b2881d1080572a
Pritesh Suvagiya <pritesh@taglineinfotech.com>
2022-02-24 17:35:34 +0530

    Create model to store request data

--- converter/models.py
+++ converter/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class RequestData(models.Model):    
+    url = models.URLField()
+    convert_from = models.CharField(max_length=10,blank=True,null=True)
+    convert_to = models.CharField(max_length=10,blank=True,null=True)
+    amount = models.FloatField(blank=True,null=True)
+    datetime = models.DateTimeField()
+    response_body = models.TextField(blank=True,null=True)
+    
+    class Meta:    
+        db_table = 'request_data'
+        verbose_name = 'Request Data'
+        verbose_name_plural = 'Request Data'
